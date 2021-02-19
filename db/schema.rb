177981636634fae7a5d7362480c792c54ee8637f
Sasha <sashapysmenny@gmail.com>
2021-02-19 17:48:32 +0200

    category image added && changed grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_142510) do
+ActiveRecord::Schema.define(version: 2021_02_19_142943) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
