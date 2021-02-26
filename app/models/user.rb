5d804ee8f26abce2770a2b04ca943f8414529da3
Sasha <sashapysmenny@gmail.com>
2021-02-26 19:00:02 +0200

    user types

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
+
+	enum role: [:student, :implementer]
+
+	validates :role, presence: true 
+
+	#after_initialize :set_default_role, :if => User.role.empty?
+
+	def permit_params
+      params.require(:user).permit(:email, :role)
+    end
+
+	def set_default_role
+		self.role ||= student
+	end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, 
