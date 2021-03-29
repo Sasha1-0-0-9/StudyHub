17647d69be73c8046f04761e82568ae7c771bc71
Sasha <sashapysmenny@gmail.com>
2021-03-29 16:28:22 +0300

    footer + orders update

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
+const { environment } = require('@rails/webpacker');
+const jquery = require('./plugins/jquery')
+const webpack = require('webpack');
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+   $: 'jquery',
+@@ -9,4 +10,5 @@ const config = environment.toWebpackConfig();
+config.resolve.alias = {
+   jquery: 'jquery/src/jquery'
+};
+environment.plugins.prepend('jquery', jquery)
+module.exports = environment; 
