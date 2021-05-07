0fe00c1e1513db59a7ea321f0aaa7dfa1197823e
Sasha <sashapysmenny@gmail.com>
2021-05-07 15:13:57 +0300

    webpach fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,9 +3,12 @@ const jquery = require('./plugins/jquery')
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
    $: 'jquery',
-@@ -9,4 +10,5 @@ const config = environment.toWebpackConfig();
+   jQuery: 'jquery',
+   Popper: ['@popperjs/core', 'default']
+}));
+const config = environment.toWebpackConfig();
 config.resolve.alias = {
    jquery: 'jquery/src/jquery'
 };
 environment.plugins.prepend('jquery', jquery)
-module.exports = environment; 
+module.exports = environment;
