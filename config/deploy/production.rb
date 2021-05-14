d72693294a09f686ac617a2d7d82fdd6c7f2cc30
Sasha <sashapysmenny@gmail.com>
2021-05-14 22:14:58 +0300

    added domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,5 +6,5 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :environment, 'production'
 set :rails_env,   'production'
 
-set :nginx_server_name, '46.101.94.118'
+set :nginx_server_name, 'studyhub.ddns.net'
 set :puma_conf, "#{shared_path}/config/puma.rb"
